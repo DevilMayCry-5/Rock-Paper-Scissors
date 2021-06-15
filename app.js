2b7f8a3152d2798ea5bd75a8e3c0749979478776
pho <phonum5@gmail.com>
2021-06-15 12:23:25 +0000

    add logic for random selection of action

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 alert("Welcome to RPS game")
+
+let actions = ["rock", "paper", "scissors"];
+
+function computerPlay() {
+    let rand = Math.floor(Math.random() * actions.length);
+    alert(actions[rand])
+}
+
+computerPlay();
